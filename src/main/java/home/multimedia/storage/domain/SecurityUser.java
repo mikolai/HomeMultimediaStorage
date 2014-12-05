4a0910f100162ade85bb53e0b0107592759108fd
Mykola <mikol@i.ua>
2014-12-06 00:48:05 +0200

    Security works

--- src/main/java/home/multimedia/storage/domain/SecurityUser.java
+++ src/main/java/home/multimedia/storage/domain/SecurityUser.java
@@ -6,24 +6,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class SecurityUser extends User implements UserDetails {
+	private final List<GrantedAuthority> authorities = new ArrayList<>();
+
 	public SecurityUser(User user) {
-		if (user != null) {
-			this.setId(user.getId());
-			this.setName(user.getName());
-			this.setPassword(user.getPassword());
-			this.addRoles(user.getRoles());
+		this.setId(user.getId());
+		this.setName(user.getName());
+		this.setPassword(user.getPassword());
+		this.addRoles(user.getRoles());
+		for (Role userRole : this.getRoles()) {
+			SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.getName());
+			authorities.add(authority);
 		}
 	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		Collection<GrantedAuthority> authorities = new ArrayList<>();
-		for (Role userRole : this.getRoles()) {
-			SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.getName());
-			authorities.add(authority);
-		}
 		return authorities;
 	}
 
